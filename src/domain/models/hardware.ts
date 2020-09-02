90204055f332f6358f5d13e85bb8199f01c4cda1
canisterism <ohtthstc@gmail.com>
2020-09-02 23:46:52 +0900

    :recycle:

---
+++ src/domain/models/hardware.ts
@@ -0,0 +1,54 @@
+export type Hardware = {
+  wikiId: number;
+  name: String;
+};
+
+export const hardwares: Hardware[] = [
+  { wikiId: 13, name: "ファミリーコンピューター" },
+  { wikiId: 14, name: "スーパーファミコン" },
+  { wikiId: 15, name: "NINTENDO64" },
+  { wikiId: 16, name: "NINTENDO GAMECUBE" },
+  { wikiId: 17, name: "Wii" },
+  { wikiId: 392, name: "WiiU" },
+  { wikiId: 665, name: "Nintendo Switch" },
+  { wikiId: 22, name: "PlayStation" },
+  { wikiId: 23, name: "PlayStation2" },
+  { wikiId: 24, name: "PlayStation3" },
+  { wikiId: 519, name: "PlayStation4" },
+  { wikiId: 25, name: "Xbox" },
+  { wikiId: 26, name: "Xbox 360" },
+  { wikiId: 556, name: "Xbox One" },
+  { wikiId: 19, name: "メガドライブ" },
+  { wikiId: 20, name: "セガサターン" },
+  { wikiId: 21, name: "ドリームキャスト" },
+  { wikiId: 30, name: "PCエンジン" },
+  { wikiId: 99, name: "ネオジオソフト" },
+  {
+    wikiId: 33,
+    name: "ゲームボーイ",
+  },
+  {
+    wikiId: 97,
+    name: "ゲームボーイアドバンス",
+  },
+  {
+    wikiId: 97,
+    name: "ニンテンドーDS",
+  },
+  {
+    wikiId: 97,
+    name: "ニンテンドー3DS",
+  },
+  {
+    wikiId: 97,
+    name: "ゲームボーイアドバンス",
+  },
+  {
+    wikiId: 97,
+    name: "プレイステーション・ポータブル",
+  },
+  {
+    wikiId: 97,
+    name: "PlayStation Vita",
+  },
+];
